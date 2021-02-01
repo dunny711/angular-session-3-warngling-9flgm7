ab5c727003f47beec9065b405febf904d9a9a333
dunny711 <daniel.duong711@gmail.com>
2021-02-01 18:08:45 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,74 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  user: User = {
-    name: null,
-    email: null,
-    password: null
-  };
+  customerList = [
+    {
+      id: 1,
+      name: "Daniel Mustermann",
+      money: -1000,
+      qty: [3, 4, 5, 120, 9],
+      sum: 0
+    },
+    {
+      id: 2,
+      name: "Emil Mustermann",
+      money: 12,
+      qty: [20, 50, 60, 10, 9],
+      sum: 0
+    },
+    {
+      id: 3,
+      name: "Bennet Mustermann",
+      money: 12,
+      qty: [20, 4, 60, 68, 9],
+      sum: 0
+    },
+    {
+      id: 4,
+      name: "Christoph Mustermann",
+      money: 12,
+      qty: [20, 50, 45, 10, 9],
+      sum: 0
+    },
+    {
+      id: 5,
+      name: "Christian Mustermann",
+      money: 12,
+      qty: [20, 50, 63, 10, 9],
+      sum: 0
+    },
+    {
+      id: 6,
+      name: "Jan Mustermann",
+      money: 12,
+      qty: [0, 50, 60, 10, 9],
+      sum: 0
+    }
+  ];
+  anonymizedNames = ["A", "B", "C", "D", "E", "F"];
+
+  anonymize() {
+    this.customerList.map(
+      (customer, i) => (customer.name = this.anonymizedNames[i])
+    );
+  }
+
+  groupByMoney() {
+    this.customerList = this.customerList.filter(customer => {
+      return customer.money > 0;
+    });
+  }
+  sumOfQuantity() {
+    // this.customerList.map(customer => {
+    //   customer.sum = customer.qty.reduce(function (accumulator, currentValue, currentIndex, array) {
+    //     return accumulator + currentValue;
+    // })
+    this.customerList.map(customer => {
+      customer.sum = customer.qty.reduce((prev, curr) => prev + curr, );
+    });
+  }
+  // }
 
   // Constructor
   constructor() {}
-
-  onSubmit() {
-    alert(
-      `User ${this.user.name} with e-mail address ${
-        this.user.email
-      } has been added to the system.`
-    );
-  }
-}
-
-export interface User {
-  name: string;
-  email: string;
-  password: string;
 }
