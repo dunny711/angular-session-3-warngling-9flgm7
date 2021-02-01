cdd5f0cc3282b81cba63b10cb731c4a93b1e2063
dunny711 <daniel.duong711@gmail.com>
2021-02-01 22:46:54 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,66 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  customerWithMoney = [
+    {
+      id: 2,
+      name: "Emil Mustermann",
+      money: 12,
+      rating: 1
+    },
+    {
+      id: 3,
+      name: "Bennet Mustermann",
+      money: 12,
+      rating: 1
+    },
+    {
+      id: 4,
+      name: "Christoph Mustermann",
+      money: 12,
+      rating: 2
+    },
+    {
+      id: 5,
+      name: "Christian Mustermann",
+      money: 12,
+      rating: 1
+    },
+    {
+      id: 6,
+      name: "Jan Mustermann",
+      money: 1,
+      rating: 10
+    }
+  ];
+  customerNoMoney = [
+    {
+      id: 1,
+      name: "Daniel Mustermann",
+      money: -5,
+      rating: -1000
+    }
+  ];
   customerList = [
     {
       id: 1,
       name: "Daniel Mustermann",
       money: -1000,
-      qty: [3, 4, 5, 120, 9],
+      qty: [0, 0, 0, 0, 0],
       sum: 0
     },
     {
       id: 2,
       name: "Emil Mustermann",
       money: 12,
-      qty: [20, 50, 60, 10, 9],
+      qty: [20, 50, 60],
       sum: 0
     },
     {
       id: 3,
       name: "Bennet Mustermann",
       money: 12,
-      qty: [20, 4, 60, 68, 9],
+      qty: [68, 9],
       sum: 0
     },
     {
@@ -39,7 +79,7 @@ export class AppComponent {
       id: 5,
       name: "Christian Mustermann",
       money: 12,
-      qty: [20, 50, 63, 10, 9],
+      qty: [9],
       sum: 0
     },
     {
@@ -51,6 +91,7 @@ export class AppComponent {
     }
   ];
   anonymizedNames = ["A", "B", "C", "D", "E", "F"];
+  lotteryThreshold = 2;
 
   anonymize() {
     this.customerList.map(
@@ -69,8 +110,34 @@ export class AppComponent {
     //     return accumulator + currentValue;
     // })
     this.customerList.map(customer => {
-      customer.sum = customer.qty.reduce((prev, curr) => prev + curr, );
+      customer.sum = customer.qty.reduce((prev, curr) => prev + curr);
     });
+  }
+  testFunction1() {
+    // this.customerList = this.customerList.filter(customer => {
+    //   return customer.qty.length > this.lotteryThreshold;
+    // });
+    // this.customerWithMoney.map(
+    //   customer => (customer.rating = customer.rating * this.randomG(4))
+    // );
+    this.customerWithMoney.filter(customer => {});
+    console.log(this.customerList);
+
+    for (var i = 0; i < this.customerList.length; i++) {
+      this.customerWithMoney.forEach(element => {
+        if (this.customerList[i].qty.length > this.lotteryThreshold) {
+          element.rating = element.rating * this.randomG(4);
+        }
+      });
+    }
+  }
+
+  randomG(v: number) {
+    var r = 0;
+    for (var i = v; i > 0; i--) {
+      r += Math.random();
+    }
+    return r / v;
   }
   // }
 
